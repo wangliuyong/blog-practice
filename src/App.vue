63845f031019fc343ab320e44413fcc62eda19d3
wangliuyong <1355498705@qq.com>
2019-03-07 12:52:55 +0800

    使用grid布局，并添加基础样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <Header></Header>
-    <router-view></router-view>
+    <div id="main">
+      <router-view></router-view>
+    </div>
+    <!-- /#main -->
     <Footer></Footer>
   </div>
 </template>
@@ -9,22 +12,48 @@
 <script>
   import Header from '@/components/Header.vue'
   import Footer from '@/components/Footer.vue'
+
   export default {
     name: 'App',
-    components:{Header,Footer},
-    updated(){
+    components: {Header, Footer},
+    updated() {
       console.log(this.$router.history)
     }
   }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    display: grid;
+    grid-template-columns: 12% auto 12%;
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas: "header header header"
+                          ".      main  ."
+                          "footer footer footer";
+
+    #header {
+      grid-area: header;
+      padding-left: 12%;
+      padding-right: 12%;
+    }
+    #main {
+      grid-area: main;
+    }
+    #footer {
+      grid-area: footer;
+      padding-left: 12%;
+      padding-right: 12%;
+    }
+
+  }
+
+  @media (max-width: 768px) {
+    #app {
+      grid-template-columns: 10px auto 10px;
+      #header, #footer {
+        padding-left: 10px;
+        padding-right: 10px;
+      }
+    }
+  }
 </style>
